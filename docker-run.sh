d0c7440558a88f6514aca6b65e7169a34d7ee1e7
Suntisuk Khemphed <suntisuk.go@gmail.com>
2022-04-03 17:02:45 +0700

    [Edited] Set volume data and config on location host

--- docker-run.sh
+++ docker-run.sh
@@ -4,6 +4,7 @@ docker run -d --name="Zoneminder" \
 --net="bridge" \
 --privileged="false" \
 --shm-size="8G" \
+-p 0880:80/tcp \
 -p 8443:443/tcp \
 -p 9000:9000/tcp \
 -e TZ="Asia/Bangkok" \
@@ -11,6 +12,6 @@ docker run -d --name="Zoneminder" \
 -e PGID="100" \
 -e MULTI_PORT_START="0" \
 -e MULTI_PORT_END="0" \
--v "/mnt/cache/appdata/Zoneminder":"/config":rw \
--v "/mnt/cache/appdata/Zoneminder/data":"/var/cache/zoneminder":rw \
+-v "/mnt/appdata/Zoneminder/config":"/config":rw \
+-v "/mnt/appdata/Zoneminder/data":"/var/cache/zoneminder":rw \
 dlandon/zoneminder.machine.learning
